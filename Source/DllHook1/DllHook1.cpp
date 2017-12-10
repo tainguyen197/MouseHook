1e1a2660893b524e622ea5aa582dc6e7a5bcc6fb
AzureAD\NGUYENTRUNGTAI <tainguyen197.ntt@gmail.com>
2017-12-10 22:43:41 +0700

    hook deadline

---
+++ Source/DllHook1/DllHook1.cpp
@@ -0,0 +1,49 @@
+﻿// DllHook1.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include <Windowsx.h>
+
+#define EXPORT  __declspec(dllexport) //khi bên dịch chương trình sẽ export ra tệp tin .DLL và .Lib
+
+HHOOK hHook = NULL;
+HINSTANCE hinstLib;
+bool isHook = true;
+
+LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode == HC_ACTION)
+	{
+		if (wParam == WM_LBUTTONDOWN)
+		{
+			if (isHook){
+				return -1;
+			}
+		}
+
+		if (wParam == WM_RBUTTONDOWN && GetAsyncKeyState(VK_CONTROL) < 0)
+		{
+			isHook = !isHook;
+		}
+		
+	}
+	return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
+EXPORT void _doInstallHook(HWND hWnd)
+{
+	if (hHook != NULL) return;
+
+	hHook = SetWindowsHookEx(WH_MOUSE_LL, (HOOKPROC)MouseHookProc, hinstLib, 0);
+	if (hHook)
+		MessageBox(hWnd, L"Hook đã được cài đặt", L"Thành công", 0);
+	else MessageBox(hWnd, L"Hook chưa được cài đặt", L"Thất bại", 0);
+}
+
+EXPORT void _doRemoveHook(HWND hWnd)
+{
+	if (hHook == NULL) return;
+	UnhookWindowsHookEx(hHook);
+	hHook = NULL;
+	MessageBox(hWnd, L"Hook đã được gỡ bỏ", L"Thông báo", MB_OK);
+}
---
+++ Source/TestDll2/TestDll2.cpp
@@ -0,0 +1,252 @@
+﻿// TestHookWithDll.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "TestDll2.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
+
+#define REMOVE 10000
+#define SETUP 10001
+void doInstallHook(HWND hWnd);
+void doRemoveHook(HWND hWnd);
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_TESTDLL2, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TESTDLL2));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TESTDLL2));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDC_TESTDLL2);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	HWND hWnd, hwnd;
+
+	hInst = hInstance; // Store instance handle in our global variable
+
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		400,100,300,300, NULL, NULL, hInstance, NULL);
+
+	//InitCommonControls();
+	hwnd = CreateWindowEx(0, L"STATIC", L"Cài đặt hook -->", WS_CHILD | WS_VISIBLE, 30, 50, 106, 23, hWnd, NULL, hInst, NULL);
+
+	hwnd = CreateWindowEx(0, L"STATIC", L"Gỡ cài đặt hook -->", WS_CHILD | WS_VISIBLE, 30, 150, 130, 23, hWnd, NULL, hInst, NULL);
+
+	hwnd = CreateWindowEx(0, L"BUTTON", L"Setup", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 200, 50, 50, 23, hWnd, (HMENU)SETUP, hInst, NULL);
+
+	hwnd = CreateWindowEx(0, L"BUTTON", L"Remove", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 200, 150, 60, 23, hWnd, (HMENU) REMOVE, hInst, NULL);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, SW_SHOW);
+	//ShowWindow(hWnd, SW_HIDE);
+	//	MessageBox(hWnd, L"Hook đã được cài đặt, bấm Ctrl + Chuột phải để bắt đầu", L"Thành công", 0);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+
+bool isHook = false;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	//doInstallHook(hWnd);
+
+	switch (message)
+	{
+	case WM_CTLCOLORSTATIC: // dùng để thay đổi màu lớp static
+		SetBkMode((HDC)wParam, TRANSPARENT);
+		return (LRESULT)GetStockObject(NULL_BRUSH);
+		break;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == SETUP){
+			doInstallHook(hWnd);
+			break;
+		}
+		if (LOWORD(wParam) == REMOVE){
+			doRemoveHook(hWnd);
+			break;
+		}
+		switch (wmId)
+		{
+			
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+void doInstallHook(HWND hWnd)
+{
+	// gọi hàm DLL theo kiểu Run-time
+	// Định nghĩa prototype của hàm
+	typedef VOID(*MYPROC)(HWND);
+
+	HINSTANCE hinstLib;
+	MYPROC ProcAddr;
+
+	// load DLL và lấy handle của DLL module
+	hinstLib = LoadLibrary(L"DllHook1.dll");
+	// Nếu load thành công, lấy địa chỉ của hàm DrawCircle trong DLL
+	if (hinstLib != NULL) {
+		ProcAddr = (MYPROC)GetProcAddress(hinstLib, "_doInstallHook");
+		// Nếu lấy được địa chỉ hàm, gọi thực hiện hàm
+		if (ProcAddr != NULL)
+			ProcAddr(hWnd);
+	}
+}
+
+void doRemoveHook(HWND hWnd)
+{
+	// gọi hàm DLL theo kiểu Run-time
+	// Định nghĩa prototype của hàm
+	typedef VOID(*MYPROC)(HWND);
+
+	HINSTANCE hinstLib;
+	MYPROC ProcAddr;
+
+	// load DLL và lấy handle của DLL module
+	hinstLib = LoadLibrary(L"DllHook1.dll");
+	// Nếu load thành công, lấy địa chỉ của hàm DrawCircle trong DLL
+	if (hinstLib != NULL) {
+		ProcAddr = (MYPROC)GetProcAddress(hinstLib, "_doRemoveHook");
+		// Nếu lấy được địa chỉ hàm, gọi thực hiện hàm
+		if (ProcAddr != NULL)
+			ProcAddr(hWnd);
+	}
+}
